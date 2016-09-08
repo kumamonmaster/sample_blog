7a881584bd4bbb36dfd2b404c19c6195b2c5a926
Takuma <fuusuke0715@gmail.com>
2016-09-08 08:49:57 +0000

    resoucesとset_postメソッドの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def new
   end
 
@@ -15,15 +17,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.title = params[:title]
     @post.body = params[:body]
     @post.save
@@ -31,8 +30,12 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
