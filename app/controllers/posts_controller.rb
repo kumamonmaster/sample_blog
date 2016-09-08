79e91620b860b4167e267c0a62840df6dc81eed7
Takuma <fuusuke0715@gmail.com>
2016-09-08 08:39:11 +0000

    destroyアクションの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,4 +29,10 @@ class PostsController < ApplicationController
     @post.save
     redirect_to post_path(@post.id)
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
 end
